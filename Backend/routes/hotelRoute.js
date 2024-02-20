50d4fa6e726bcce25fd39ce57d6e3bb5cc6ff0df
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-20 17:59:43 +0300

    Done

--- Backend/routes/hotelRoute.js
+++ Backend/routes/hotelRoute.js
@@ -6,9 +6,9 @@ const hotelController = require('../Controllers/hotelConroller')
 
 // get Route
 router.get('/all',hotelController.getAll)
-router.get('/filter/:id',hotelController.filteringHotels)
-router.post('/new',hotelController.createHotel)
-router.delete('/delete/:id',hotelController.deletingHotel);
-router.put('/update/:id',hotelController.updatingHotel)
+// router.get('/filter/:id',hotelController.filteringHotels)
+// router.post('/new',hotelController.createHotel)
+// router.delete('/delete/:id',hotelController.deletingHotel);
+// router.put('/update/:id',hotelController.updatingHotel)
 
 module.exports = router;
